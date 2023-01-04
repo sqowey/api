5e004a099596b03dc12f0699b6a9d8481e9b82be
CuzImBisonratte <cuzimbisonratte@gmail.com>
2023-01-04 18:10:33 +0200

    Add wildcard + tls/ssl support

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
+const config = require("./config.json");
+const https = require('https');
+const fs = require('fs');
 
 // Bring in the routes for /helloworld and /helloworld/*/hw2
 const helloWorldRoute = require('./routes/helloworld');
 const hwRoute = require('./routes/hw2');
 
-// Use the routes for their respective endpoints
-app.use('/helloworld', helloWorldRoute);
-app.use('/helloworld', hwRoute);
-
-app.listen(3000, () => {
-    console.log('Server listening on port 3000');
+// Wildcard route
+app.get('*', (req, res) => {
+    res.send('Route not found!');
 });
+
+// Listener
+if (config.secure) https.createServer({
+    key: fs.readFileSync('/etc/letsencrypt/live/api.sqowey.de/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/api.sqowey.de/fullchain.pem')
+}, app).listen(443);
+else app.listen(config.port, () => console.log('Server listening on port ' + config.port));
